796b19a0c3f8b70373d5f79bc7f5046fb0ca0f92
vscalcione <vincenzo99.scalcione@gmail.com>
2020-04-12 19:00:24 +0200

    Add palindrome_word.py script

---
+++ exercises/palindrome_word.py
@@ -0,0 +1,31 @@
+from pip._vendor.distlib.compat import raw_input
+import os
+import time
+
+
+def is_palindrome_word():
+    word = raw_input("Insert word: ")
+    tmp_word = ''.join(reversed(word))
+    if word == tmp_word:
+        print("Entered word: " + word + " is a palindrome word ")
+    else:
+        print("Entered word: " + word + " isn't a palindrome word ")
+
+
+def print_menu():
+    print("==================================================")
+    print(" Palindrome Word")
+    print("==================================================")
+
+
+if __name__ == "__main__":
+    while True:
+        print_menu()
+        # time.sleep(2) This instruction can put on sleep mode the execution of all script
+        is_palindrome_word()
+        choice = raw_input("Another operation? yes/no ")
+        if choice == "yes" or choice == "y":
+            os.system("clear")
+            is_palindrome_word()
+        else:
+            break
