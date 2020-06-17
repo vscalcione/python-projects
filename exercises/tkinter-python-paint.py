dd8beae626336c12954ae29df7fee98484206f09
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-06-17 16:44:12 +0200

    Add brush parameters on tkinter-python-paint.py

---
+++ exercises/tkinter-python-paint.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+root = Tk()
+root.title('Python Tkinter Paint')
+root.geometry("800x800")
+
+def paint(event):
+
+    # Brush params
+    brush_width = 20
+    brush_color = "green"
+    
+    # Brush Type: BUTT, ROUND, PROJECTING
+    brush_type = PROJECTING
+
+    # Starting position
+    x1 = event.x - 1
+    y1 = event.y - 1
+
+    # Ending position
+    x2 = event.x + 1
+    y2 = event.y + 1
+
+    # Draw on the canvas
+    my_canvas.create_line(x1, y1, x2, y2, fill=brush_color, width=brush_width, capstyle=brush_type, smooth=True)
+
+w = 600
+h = 400
+
+my_canvas = Canvas(root, width=w, height=h, bg="white")
+my_canvas.pack(pady=20)
+
+# my_canvas.create_line(0, 100, 300, 100, fill="red")
+# my_canvas.create_line(150, 0, 150, 200, fill="red")
+my_canvas.bind('<B1-Motion>', paint)
+
+root.mainloop()
+
