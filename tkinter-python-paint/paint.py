ac7f9ba7cd7f96ac148952ec76f60eea9fddddd8
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-06-17 13:49:23 +0200

    Add python paint

--- tkinter-python-paint/paint.py
+++ tkinter-python-paint/paint.py
@@ -7,15 +7,11 @@ root.geometry("800x800")
 w = 600
 h = 400
 
-x1 = 0
-x2 = 100
-y1 = 300
-y2 = 100
-
 my_canvas = Canvas(root, width=w, height=h, bg="white")
 my_canvas.pack(pady=20)
 
-my_canvas.create_line(x1, y1, x2, y2, fill="red")
+my_canvas.create_line(0, 100, 300, 100, fill="red")
+my_canvas.create_line(150, 0, 150, 200, fill="red")
 
 root.mainloop()
 
